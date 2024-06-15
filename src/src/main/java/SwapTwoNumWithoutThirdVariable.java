5fd0af33b22c3d09c57edbdbc87b32acfd00c198
suriya ss <ss.suriyastark@gmail.com>
2024-06-15 22:12:39 +0530

    swap two number without third variable

--- src/src/main/java/SwapTwoNumWithoutThirdVariable.java
+++ src/src/main/java/SwapTwoNumWithoutThirdVariable.java
@@ -1,5 +1,31 @@
 public class SwapTwoNumWithoutThirdVariable {
     public static void main(String args[]){
- 
+        int nOne =9;
+        int nTwo=5;
+       System.out.println("Before swap::::" +
+               "NUMBER ONE ="+nOne+"" +
+               " NUMBER TWO = "+nTwo+"");
+
+       /*way------------arthemetic operator*/
+
+        nOne = nOne +nTwo;
+        nTwo = nOne - nTwo;
+        nOne = nOne - nTwo;
+        System.out.println("After swap::: arthemetic:::" +
+                "NUMBER ONE ="+nOne+"" +
+                " NUMBER TWO = "+nTwo+"");
+
+        /*way---------------multi/divi*/
+
+        nOne =9;
+        nTwo=5;
+        nOne = nOne* nTwo;
+        nTwo = nOne/nTwo;
+        nOne = nOne/nTwo;
+
+        System.out.println("After swap::: multi/divi:::" +
+                "NUMBER ONE ="+nOne+"" +
+                " NUMBER TWO = "+nTwo+"");
+
     }
 }
